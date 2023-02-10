6408fc61eee789c1c78bcaf28d01f517b8262f9b
Darel <darelbavcar1@gmail.com>
2023-02-10 17:52:53 +0100

    styled navbar

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import styled from "styled-components"
+
+
+
+const Home = () => {
+    return (
+  
+            <Navbar />
+   
+    )
+}
+
+export default Home
+
