02b4e8f7dbfaf2dcfbcdd8535d06b97f4009bb8d
Darel <darelbavcar1@gmail.com>
2023-02-13 14:56:01 +0100

    finished home page

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,26 +1,21 @@
-
-import styled from "styled-components"
-import { categories } from "../data"
-import { CategoryItem } from "./CategoryItem"
-
-
+import styled from "styled-components";
+import { categories } from "../data";
+import { CategoryItem } from "./CategoryItem";
 
 const Categories = () => {
+  return (
+    <Container>
+      {categories.map((item) => (
+        <CategoryItem key={item.id} item={item} />
+      ))}
+    </Container>
+  );
+};
 
-
-
-    return (
-        <Container>
-            {categories.map((item) =>
-                <CategoryItem key= {item.id} item = {item}/>)}
-        </Container>
-    )
-}
-
-export default Categories
+export default Categories;
 
 const Container = styled.div`
-display: flex;
-padding: 20px;
-justify-content: space-between;
-`
+  display: flex;
+  padding: 20px;
+  justify-content: space-between;
+`;
